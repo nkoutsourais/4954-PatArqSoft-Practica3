5fd0f9bac43d857ef63cb502adf2ac9d28962570
KOUTSOURAIS Neofitos <neofitos.KOUTSOURAIS@aldautomotive.com>
2019-10-14 10:33:22 +0200

    Refactoring

--- src/mastermind/MastermindStandalone.java
+++ src/mastermind/MastermindStandalone.java
@@ -1,14 +1,26 @@
 package mastermind;
 
+import mastermind.controllers.Logic;
 import mastermind.controllers.implementation.LogicImplementation;
 import mastermind.models.dao.DaoType;
 
 public class MastermindStandalone extends Mastermind {
 
+    DaoType daoType = DaoType.FILE;
+
+    public MastermindStandalone(DaoType daoType) {
+        this.daoType = daoType;
+    }
+
     public static void main(String[] args) {
         DaoType daoType = DaoType.FILE;
-        if(args.length > 0 && args[0] != null)
+        if (args.length > 0 && args[0] != null)
             daoType = DaoType.parser(args[0]);
-        new MastermindStandalone().play(new LogicImplementation(daoType));
+        new MastermindStandalone(daoType).play();
+    }
+
+    @Override
+    protected Logic createLogic() {
+        return new LogicImplementation(this.daoType);
     }
 }
--- src/mastermind/distributed/MastermindClient.java
+++ src/mastermind/distributed/MastermindClient.java
@@ -5,13 +5,21 @@ import mastermind.Mastermind;
 
 public class MastermindClient extends Mastermind {
 
+    private LogicProxy logicProxy;
+
     public static void main(String[] args) {
-        new MastermindClient().play(new LogicProxy());
+        new MastermindClient().play();
     }
 
     @Override
-    protected void play(Logic logic) {
-        super.play(logic);
-        ((LogicProxy)logic).close();
+    protected Logic createLogic() {
+        this.logicProxy = new LogicProxy();
+        return this.logicProxy;
+    }
+
+    @Override
+    protected void play() {
+        super.play();
+        this.logicProxy.close();
     }
 }
